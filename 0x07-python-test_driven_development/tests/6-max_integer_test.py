109438b32b23b751d21926e1e5958f84e82b6066
Tayo Jubril <jubriltayo@gmail.com>
2023-01-10 06:51:07 -0800

    task 5 updated

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,61 +1,44 @@
 #!/usr/bin/python3
-"""Unittests for max_integer([..])."""
-
+"""Unittest for max_integer([..])
+"""
 import unittest
 max_integer = __import__('6-max_integer').max_integer
 
 
 class TestMaxInteger(unittest.TestCase):
-    """Define unittests for max_integer([..])."""
+    """Tests different scenarios"""
 
-    def test_ordered_list(self):
-        """Test an ordered list of integers."""
-        ordered = [1, 2, 3, 4]
-        self.assertEqual(max_integer(ordered), 4)
+    def test_end(self):
+        """Test for max if at the end"""
+        sample = [1, 2, 3, 4, 5]
+        self.assertEqual(max_integer(sample), 5)
 
-    def test_unordered_list(self):
-        """Test an unordered list of integers."""
-        unordered = [1, 2, 4, 3]
-        self.assertEqual(max_integer(unordered), 4)
+    def test_beginning(self):
+        """Test for max if at the beginning"""
+        sample = [7, 2, 3, 4, 1]
+        self.assertEqual(max_integer(sample), 7)
 
-    def test_max_at_begginning(self):
-        """Test a list with a beginning max value."""
-        max_at_beginning = [4, 3, 2, 1]
-        self.assertEqual(max_integer(max_at_beginning), 4)
+    def test_middle(self):
+        """Test for max if at the middle"""
+        sample = [5, 2, 9, 4, 1]
+        self.assertEqual(max_integer(sample), 9)
 
-    def test_empty_list(self):
-        """Test an empty list."""
-        empty = []
-        self.assertEqual(max_integer(empty), None)
+    def test_negative(self):
+        """Test for max if one negative value exists"""
+        sample = [5, -2, 9, 4, 1]
+        self.assertEqual(max_integer(sample), 9)
 
-    def test_one_element_list(self):
-        """Test a list with a single element."""
-        one_element = [7]
-        self.assertEqual(max_integer(one_element), 7)
+    def test_allnegative(self):
+        """Test for max if all are negative values"""
+        sample = [-5, -2, -9, -4, -1]
+        self.assertEqual(max_integer(sample), -1)
 
-    def test_floats(self):
-        """Test a list of floats."""
-        floats = [1.53, 6.33, -9.123, 15.2, 6.0]
-        self.assertEqual(max_integer(floats), 15.2)
+    def test_one_element(self):
+        """Test for max if only one element exist"""
+        sample = [5]
+        self.assertEqual(max_integer(sample), 5)
 
-    def test_ints_and_floats(self):
-        """Test a list of ints and floats."""
-        ints_and_floats = [1.53, 15.5, -9, 15, 6]
-        self.assertEqual(max_integer(ints_and_floats), 15.5)
-
-    def test_string(self):
-        """Test a string."""
-        string = "Tayo"
-        self.assertEqual(max_integer(string), 'r')
-
-    def test_list_of_strings(self):
-        """Test a list of strings."""
-        strings = ["Tayo", "is", "my", "name"]
-        self.assertEqual(max_integer(strings), "name")
-
-    def test_empty_string(self):
-        """Test an empty string."""
-        self.assertEqual(max_integer(""), None)
-
-if __name__ == '__main__':
-    unittest.main()
+    def test_void(self):
+        """Test for max if void"""
+        sample = []
+        self.assertEqual(max_integer(sample), None)
