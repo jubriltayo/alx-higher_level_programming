3917f7c8e25ece96c05e7a64b628274fda447374
Tayo Jubril <jubriltayo@gmail.com>
2023-01-12 01:16:38 -0800

    task solution updated

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -76,6 +76,7 @@ class Rectangle:
 
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
+        """Returns the biggest rectangle based on the area"""
         if not isinstance(rect_1, Rectangle):
             raise TypeError("rect_1 must be an instance of Rectangle")
         if not isinstance(rect_2, Rectangle):
@@ -86,4 +87,5 @@ class Rectangle:
 
     @classmethod
     def square(cls, size=0):
+        """Returns a new Rectangle instance"""
         return cls(size, size)
