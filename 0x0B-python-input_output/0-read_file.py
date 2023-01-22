94c428a10ee527c6d11b4c17514b645195261c6c
Tayo Jubril <jubriltayo@gmail.com>
2023-01-22 21:00:40 +0100

    task solution update

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """Module to read file and prints it to stdout"""
-import json
 
 
 def read_file(filename=""):
     """Reads content of a file and prints"""
+
     with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
