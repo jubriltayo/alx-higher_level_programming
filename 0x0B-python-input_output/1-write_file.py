d6eb9792ea9f4857723672a4cf50bc4ecc330be4
Tayo Jubril <jubriltayo@gmail.com>
2023-01-22 21:02:40 +0100

    task solution update

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """Module that writes a string into a file"""
-import json
 
 
 def write_file(filename="", text=""):
     """Writes a string into a file and returns
 the number of characters
     """
+
     with open(filename, 'w', encoding="utf-8") as f:
         return f.write(text)
--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """Module to append string to a file"""
-import json
 
 
 def append_write(filename="", text=""):
     """Appends string and returns the number of
 characters added
     """
+
     with open(filename, 'a', encoding="utf-8") as f:
         return f.write(text)
