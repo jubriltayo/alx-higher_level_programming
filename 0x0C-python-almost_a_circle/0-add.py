2026166924f1ccef1d0b44d453350b257d197e9b
Tayo Jubril <jubriltayo@gmail.com>
2023-01-23 00:48:32 +0100

    testing

---
+++ 0x0C-python-almost_a_circle/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Module to test additon of numbers"""
+
+
+def add(a, b):
+    """Returns the sum of two integers"""
+    return int(a) + int(b)
---
+++ 0x0C-python-almost_a_circle/0-main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+
+add = __import__('0-add').add
+if __name__ == "__main__":
+
+sum = add(5, 7)
+print(sum)
