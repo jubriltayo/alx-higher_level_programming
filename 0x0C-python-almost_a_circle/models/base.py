c5ed945bc7527cd8139ac7c54efc35979f9e9ba0
Tayo Jubril <jubriltayo@gmail.com>
2023-01-24 00:52:04 +0100

    tasks solution update

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Module to create a class: Base"""
+import json
 
 
 class Base:
@@ -14,3 +15,40 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Returns JSON string"""
+        if list_dictionaries:
+            return json.dumps(list_dictionaries)
+        else:
+            return "[]"
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Writes JSON string to a file"""
+        filename = cls.__name__ + '.json'
+        with open(filename, 'w', encoding="utf-8") as f:
+            if not list_objs:
+                result = []
+            else:
+                result = [i.to_dictionary() for i in list_objs]
+            f.write(Base.to_json_string(result))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """Returns list of JSON string"""
+        if json_string:
+            return json.loads(json_string)
+        else:
+            return []
+
+    @classmethod
+    def create(cls, **dictionary):
+        """Returns an instance with all attributes"""
+        if cls.__name__ == 'Rectangle':
+            shape = cls(1, 1)
+        else:
+            shape = cls(1)
+        shape.update(**dictionary)
+        return shape
