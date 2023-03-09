242b4dbbee0c6cba92287224e939e1cfaae2567e
Tayo Jubril <jubriltayo@gmail.com>
2023-03-09 19:07:39 +0100

    tasks 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        for column in row:
-            if column == row[-1]:
-                print('{:d}'.format(column), end='')
-            else:
-                print('{:d}'.format(column), end=' ')
-        print()
+        if len(row) != 0:
+            for i in range(len(row)):
+                print("{:d}".format((row[i])), end=' ')
+                if (i + 1) % 3 == 0:
+                    print()
+        else:
+            print()
