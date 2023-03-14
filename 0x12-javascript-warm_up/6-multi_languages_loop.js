8c4f60e9fc5115ccc7aee61fcb580ed38fee3e3c
Tayo Jubril <jubriltayo@gmail.com>
2023-03-14 01:56:41 +0100

    Task 6

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const myLines = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const myLines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (let i = 0; i < 3; i++) {
   console.log(myLines[i]);
 }
