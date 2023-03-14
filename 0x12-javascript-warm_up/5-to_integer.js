4d5b1fcafc4ef0d71c4766903a8a8cb4368ef5eb
Tayo Jubril <jubriltayo@gmail.com>
2023-03-14 01:47:33 +0100

    Task 5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -4,5 +4,5 @@ const args = Math.floor(Number(process.argv[2]));
 if (isNaN(args)) {
   console.log('Not a number');
 } else {
-  console.log("My number: " + args);
+  console.log('My number: ' + args);
 }
