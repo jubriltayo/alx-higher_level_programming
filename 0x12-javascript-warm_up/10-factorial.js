807a5970eb56232878fb8db17294a131a62d20a8
Tayo Jubril <jubriltayo@gmail.com>
2023-03-14 08:29:50 +0100

    Task 10

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const args = Number(process.argv[2]);
+
+function factorial (num) {
+  if ((num === 0) || (isNaN(num))) {
+    return 1;
+  } else {
+    return (num * factorial(num - 1));
+  }
+}
+console.log(factorial(args));
