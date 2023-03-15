9b3316775e0c11a5c717179dd2e990d9af3a8051
Tayo Jubril <jubriltayo@gmail.com>
2023-03-15 10:36:37 +0100

    Task 6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -7,16 +7,16 @@ module.exports = class Square extends Rectangle {
   }
 
   charPrint (c) {
-    for (let i = 0; i < this.height; i++) {
-      let line = '';
-      for (let j = 0; j < this.width; j++) {
-        if (c) {
+    if (c === undefined) {
+      this.print();
+    } else {
+      for (let i = 0; i < this.height; i++) {
+        let line = '';
+        for (let j = 0; j < this.width; j++) {
           line += 'C';
-        } else {
-          line += 'X';
         }
+        console.log(line);
       }
-      console.log(line);
     }
   }
 };
