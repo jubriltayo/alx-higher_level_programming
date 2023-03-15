743ce79347159ea2d125375af3c6b5b9e3f97fcf
Tayo Jubril <jubriltayo@gmail.com>
2023-03-15 10:30:54 +0100

    Task 6

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+const Rectangle = require('./4-rectangle');
+
+module.exports = class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    for (let i = 0; i < this.height; i++) {
+      let line = '';
+      for (let j = 0; j < this.width; j++) {
+        if (c) {
+          line += 'C';
+        } else {
+          line += 'X';
+        }
+      }
+      console.log(line);
+    }
+  }
+};
