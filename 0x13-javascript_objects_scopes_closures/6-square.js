389a01046ac906293cf3b9efe9453299fa942d29
Tayo Jubril <jubriltayo@gmail.com>
2023-03-15 10:42:36 +0100

    Task 6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,11 +1,7 @@
 #!/usr/bin/node
-const Rectangle = require('./4-rectangle');
+const SquareMain = require('./5-square');
 
-module.exports = class Square extends Rectangle {
-  constructor (size) {
-    super(size, size);
-  }
-
+module.exports = class Square extends SquareMain {
   charPrint (c) {
     if (c === undefined) {
       this.print();
