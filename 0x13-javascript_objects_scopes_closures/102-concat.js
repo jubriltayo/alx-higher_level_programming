bfd64b9298ceabe71fe73947cbada3ec408b80e4
Tayo Jubril <jubriltayo@gmail.com>
2023-03-16 17:17:52 +0100

    task 13

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 const fs = require('fs');
 
-const fileA = fs.readFileSync(process.argv[2], 'utf8') + '\n';
-const fileB = fs.readFileSync(process.argv[3], 'utf8') + '\n';
+const fileA = fs.readFileSync(process.argv[2], 'utf8');
+const fileB = fs.readFileSync(process.argv[3], 'utf8');
 const result = fileA + fileB;
 
 fs.writeFileSync(process.argv[4], result);
