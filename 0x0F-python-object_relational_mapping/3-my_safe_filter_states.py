88e8206fd547a2a4e24c1d8e6dabd7b373e1665f
Tayo Jubril <jubriltayo@gmail.com>
2023-03-18 19:03:38 +0100

    task 3

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+This script takes in an argument and prints
+the states that matches the argument
+from the database hbtn_0e_0_usa
+"""
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    """
+    Connects to database and fetches information from states
+    """
+    conn = MySQLdb.connect(
+        host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3])
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states\
+            WHERE name LIKE BINARY %(name)s\
+            ORDER BY states.id", {'name': argv[4]})
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
