57dd2b77b04fdf9bb60b6e4c71c83bc2bb2f5762
Tayo Jubril <jubriltayo@gmail.com>
2023-03-22 10:04:49 +0100

    task 14

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+This module defines a child class(City) inherited
+from a parent class(Base) and links to MYSQL table cities
+"""
+
+from model_state import Base
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Integer, Column, String, ForeignKey
+
+
+class City(Base):
+    """
+    Class City represents a city columns for mySQL table
+
+    __tablename__: name of table
+    id: city id
+    name: city name
+    state_id: reference to states.id(Foreign Key)
+    """
+    __tablename__ = "cities"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
