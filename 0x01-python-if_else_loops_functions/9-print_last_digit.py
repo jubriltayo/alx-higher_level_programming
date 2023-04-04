7aa45c81254c0a9354aa28fa8ea52a2bb063c96e
Tayo Jubril <jubriltayo@gmail.com>
2023-04-04 08:49:14 +0100

    task 0

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-def print_last_digit(number):
-    if number < 0:
-        lastd = number % -10
-        print(-(lastd), end="")
-    else:
-        lastd = number % 10
-        print((lastd), end="")
-    return abs(lastd)
--- 0x03-python-data_structures/5-no_c.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-
-def no_c(my_string):
-    new_string = ''
-    for x in my_string:
-        if x != 'c' and x != 'C':
-            new_string = new_string + x
-    return new_string
