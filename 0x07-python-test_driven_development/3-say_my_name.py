d9c31948a674653bf12570748fbfb870582e9c45
Tayo Jubril <jubriltayo@gmail.com>
2023-06-08 00:00:56 +0100

    task update

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -12,10 +12,10 @@ def say_my_name(first_name, last_name=""):
     Args:
         first_name (str): The fisrt name to be printed
         last_name (str): The last name to be printed
-    
+
     Raises:
         TypeError: If either the first_name and last_name are not strings
-    
+
     '''
 
     if not isinstance(first_name, str):
