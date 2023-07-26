7756c8dc775773292fcb6f2914dc05790707eeef
Tayo Jubril <jubriltayo@gmail.com>
2023-07-26 03:37:14 +0100

    4 - fix

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -4,14 +4,14 @@
 
 const request = require('request');
 const url = process.argv[2];
-const actor = 'https://swapi-api.alx-tools.com/api/people/18/'
+const actor = 'https://swapi-api.alx-tools.com/api/people/18/';
 
 request(url, (err, res, data) => {
   if (err) {
     console.log(err);
   } else {
-    const films = JSON.parse(data).results; //results unpack the array
-    const match = films.filter((film) => film.characters.includes(actor))
+    const films = JSON.parse(data).results; // results unpack the array
+    const match = films.filter((film) => film.characters.includes(actor));
     console.log(`${match.length}`);
   }
 });
