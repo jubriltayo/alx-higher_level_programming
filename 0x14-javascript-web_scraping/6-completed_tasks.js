4b73d92abec78ed20b1a1120f852273f6939165e
Tayo Jubril <jubriltayo@gmail.com>
2023-07-27 17:53:04 +0100

    task 6 updated

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -5,7 +5,7 @@
 const request = require('request');
 const url = process.argv[2];
 
-request(url, (err, res, data) => {
+request.get(url, (err, res, data) => {
   if (err) {
     console.log(err);
   } else {
