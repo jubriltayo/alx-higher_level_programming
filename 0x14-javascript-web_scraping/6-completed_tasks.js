1754ffe901d7d7f57bb946976452c85122900878
Tayo Jubril <jubriltayo@gmail.com>
2023-07-27 17:57:26 +0100

    task 6 updated

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -8,19 +8,19 @@ const url = process.argv[2];
 request.get(url, (error, response, data) => {
   if (error) {
     console.error(error);
-  } else {
-    const todos = JSON.parse(data);
-    const completedTasks = {};
-
-    todos.forEach(todo => {
-      if (todo.completed) {
-        if (completedTasks[todo.userId]) {
-          completedTasks[todo.userId]++;
-        } else {
-          completedTasks[todo.userId] = 1;
-        }
-      }
-    });
-    console.log(completedTasks);
+    return;
   }
+  const todos = JSON.parse(data);
+  const completedTasks = {};
+
+  todos.forEach(todo => {
+    if (todo.completed) {
+      if (completedTasks[todo.userId]) {
+        completedTasks[todo.userId]++;
+      } else {
+        completedTasks[todo.userId] = 1;
+      }
+    }
+  });
+  console.log(completedTasks);
 });
